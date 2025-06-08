5fb525ae4860eec8895eb8bb140f3565ef3b62b2
Abram <abram.morris62@gmail.com>
2025-06-08 19:26:25 +0300

    Update League and Sports UI components and view controllers

--- Sporix/View/LeagueDetails/Components/FixtureCollectionViewCell.swift
+++ Sporix/View/LeagueDetails/Components/FixtureCollectionViewCell.swift
@@ -18,7 +18,36 @@ class FixtureCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var teamImage2: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        updateBackground(for: traitCollection)
+
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateBackground(for: traitCollection)
+    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+            updateBackground(for: traitCollection)
+            setNeedsLayout()
+        }
     }
 
+    private func updateBackground(for traitCollection: UITraitCollection) {
+        if traitCollection.userInterfaceStyle == .dark {
+            self.backgroundColor = .white
+            matchTime.textColor = .black
+            matchDate.textColor = .black
+            matchResult.textColor = .black
+            team2Name.textColor = .black
+            team1Name.textColor = .black
+        } else {
+            self.backgroundColor =  UIColor(red: 162/255, green: 192/255, blue: 200/255, alpha: 1) // #a2c0c8
+            matchTime.textColor = .label
+            matchDate.textColor = .label
+            matchResult.textColor = .label
+            team2Name.textColor = .label
+            team1Name.textColor = .label
+        }
+    }
 }
